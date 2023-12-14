83e6cefeef32c4285d23e928086bb30e8979951e
Magnus Mutai <MagnusMq07@gmail.com>
2023-12-14 11:23:07 +0300

    Add project files.

---
+++ DapperSyncfusion/Data/Bug.cs
@@ -0,0 +1,15 @@
+﻿namespace DapperSyncfusion.Data
+{
+    public class Bug
+    {
+        public int Id { get; set; }
+        public string Summary { get; set; }
+        public string BugPriority { get; set; }
+        public string Assignee { get; set; }
+        public string BugStatus { get; set; }
+        
+        
+        
+        
+    }
+}
---
+++ DapperSyncfusion/Data/BugDataAdaptor.cs
@@ -0,0 +1,21 @@
+﻿using Syncfusion.Blazor.Data;
+using Syncfusion.Blazor;
+
+namespace DapperSyncfusion.Data
+{
+    public class BugDataAdaptor : DataAdaptor
+    {
+        private BugDataAccessLayer _dataLayer;
+        public BugDataAdaptor(BugDataAccessLayer bugDataAccessLayer)
+        {
+            _dataLayer = bugDataAccessLayer;
+        }
+
+        public override async Task<object> ReadAsync(DataManagerRequest dataManagerRequest, string key = null)
+        {
+            List<Bug> bugs = await _dataLayer.GetBugsAsync();
+            int count = await _dataLayer.GetBugCountAsync();
+            return dataManagerRequest.RequiresCounts ? new DataResult() { Result = bugs, Count = count } : count;
+        }
+    }
+}
